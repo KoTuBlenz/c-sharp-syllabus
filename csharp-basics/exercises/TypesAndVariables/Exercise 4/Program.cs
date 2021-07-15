beb9c8fcdfdaf028ea298cbc7a29572e9aaf3d41
KrEdu <kristians12x@gmail.com>
2021-07-15 17:04:29 +0300

    Finished exercise #4

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise 4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "";
+            int birthYear;
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("What is your birth year?");
+            Int32.TryParse(Console.ReadLine(), out birthYear);
+            Console.WriteLine($"My name is {name} and I was born in {birthYear}.");
+            Console.ReadKey();
+        }
+    }
+}
