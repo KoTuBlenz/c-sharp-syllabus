6c9dc56ff7a4a60720fe2550d603980ca8d5e48c
KrEdu <kristians12x@gmail.com>
2021-07-16 17:46:53 +0300

    Exercise5 and Exercise6 Done

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumOfNumbers=0;
+            Console.WriteLine("Write a number to add, or press (N) to see result!");
+            while (true)             
+            {
+                string keyPressed = Console.ReadLine();
+                if (keyPressed == "N" || keyPressed == "n") break;
+                if (int.TryParse(keyPressed, out int number)) sumOfNumbers+=number;
+            }
+            Console.WriteLine(sumOfNumbers);
+            Console.ReadKey();
+        }
+    }
+}
