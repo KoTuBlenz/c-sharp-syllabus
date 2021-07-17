6b605138269e5b6c0c3c70cebb4ddfd8529de2ac
KrEdu <kristians12x@gmail.com>
2021-07-17 16:58:56 +0300

    Exercise5 done

---
+++ csharp-basics/exercises/Arithmetic/Exercise5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm thinking of a number between 1-100.  Try to guess it.");
+            var rnd = new Random();
+            int randomNumber = rnd.Next(1, 100);
+            int.TryParse(Console.ReadLine(), out int numberGuessed);
+            string answer = "";
+            if (numberGuessed == randomNumber)
+            {
+                answer = "You guessed it!  What are the odds?!?";
+            }
+            else if (numberGuessed > randomNumber)
+            {
+                answer = $"Sorry, you are too high.  I was thinking of {randomNumber}";
+            }
+            else
+            {
+                answer = $"Sorry, you are too low.  I was thinking of {randomNumber}";
+            }
+            Console.WriteLine(answer);
+            Console.ReadKey();
+
+        }
+    }
+}
