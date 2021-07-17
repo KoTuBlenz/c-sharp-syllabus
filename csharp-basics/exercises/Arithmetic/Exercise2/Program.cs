8072163753ee1511d9fe0f8e72565cd62551e825
KrEdu <kristians12x@gmail.com>
2021-07-17 16:54:44 +0300

    Exercise2 done

---
+++ csharp-basics/exercises/Arithmetic/Exercise2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number!");
+            int.TryParse(Console.ReadLine(), out int number);
+            if (number % 2 == 0) Console.WriteLine("Even number");
+            else Console.WriteLine("Odd number");
+            Console.ReadKey();
+            Console.WriteLine("Bye!");
+            Console.ReadKey();
+        }
+    }
+}
