deba0732740534bc3239381aa4737f7294276f21
KrEdu <kristians12x@gmail.com>
2021-07-17 17:05:08 +0300

    Exercise4 done

---
+++ csharp-basics/exercises/Arithmetic/Product1ToN/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product1ToN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number N");
+            int.TryParse(Console.ReadLine(), out int number);
+            int factorialNumber=1;
+            for (int i=1; i<=number; i++)
+            {
+                factorialNumber *= i;
+            }
+            Console.WriteLine($"The factorial of number {number} is {factorialNumber}");
+            Console.ReadKey();
+        }
+    }
+}
