ab7bedf5e171f516e8ce68cf5ab853908a8dc69e
KrEdu <kristians12x@gmail.com>
2021-07-18 13:08:44 +0300

    Added some necessary gaps and {} for better readability

--- csharp-basics/exercises/TypesAndVariables/Exercise5/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/Exercise5/Program.cs
@@ -21,6 +21,7 @@ namespace Exercise5
                 "Ms. Johannessen",
                 "Mr. James"
             };
+
             string[] classNames = new string[]
             {
                 "English III",
@@ -32,40 +33,59 @@ namespace Exercise5
                 "AP US History",
                 "Business Computer Infomation Systems"
             };
+
             int tableSize = classNames.Length;
             int longestClassNameLength = 0;
             int longestTeacherNameLength = 0;
             int tableNumberLength = tableSize.ToString().Length;
+
             foreach (string className in classNames)        //Finds longest name of a class
             {
                 if (className.Length > longestClassNameLength) longestClassNameLength = className.Length;
             }
+
             foreach (string teacherName in teachers)        // Finds longest name of a teahcer.
             {
                 if (teacherName.Length > longestTeacherNameLength) longestTeacherNameLength = teacherName.Length;
             }
+
             for (int i = 0; i < tableSize; i++)             // Makes all class name entries the same size
             {
                 int lengthDiference = longestClassNameLength - classNames[i].Length;
                 string spaceBeforWord = "";
-                for (int k = 0; k < lengthDiference; k++) spaceBeforWord += " ";
+                for (int k = 0; k < lengthDiference; k++)
+                {
+                    spaceBeforWord += " ";
+                }
+
                 classNames[i] = spaceBeforWord + classNames[i];
             }
+
             for (int i = 0; i < tableSize; i++)                                     // Makes all teacher name entries the same size. 
             {                                                                    
                 int lengthDiference = longestTeacherNameLength - teachers[i].Length;
                 string spaceBeforWord = "";
-                for (int k = 0; k < lengthDiference; k++) spaceBeforWord += " ";
+                for (int k = 0; k < lengthDiference; k++)
+                {
+                    spaceBeforWord += " ";
+                }
+
                 teachers[i] = spaceBeforWord + teachers[i];
             }
             int lineTotalLength = longestClassNameLength + longestTeacherNameLength + tableNumberLength + 8; // Magic number 8.
                                                                                                              // This number is for
                                                                                                              // the spaces in the table                                                                                                            
-            string tableEnds = "+";
-            for (int i = 0; i < lineTotalLength; i++) tableEnds += "-";
-            tableEnds += "+";
+            string tableEnds = "+";    // This is the tables first and last line
+            for (int i = 0; i < lineTotalLength; i++)
+            {
+                tableEnds += "-";
+            }
+
+            tableEnds += "+";       //Adds '+' sing to the end of the line
+
             Console.WriteLine(tableEnds);
-            for (int i = 0; i < tableSize; i++)
+
+            for (int i = 0; i < tableSize; i++)         //Loop that writes the table itself from given
             {
                 string tableNumber = (i + 1).ToString();
                 while (true)
