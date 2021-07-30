7c5746bfcf2aff4c45660b2ce156419c74b65fd8
KrEdu <kristians12x@gmail.com>
2021-07-30 12:52:22 +0300

    Exercise9 done

---
+++ csharp-basics/exercises/Loops/Loops/Exercise9/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercise9
+{
+    class RollTwoDice
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter desired sum (2-12):");
+            var rand = new Random();
+            int desirableSum;
+            while (true)        //Checks users input
+            {
+                if (int.TryParse(Console.ReadLine(), out desirableSum))                   
+                {
+                    if (desirableSum > 12 || desirableSum < 0)
+                    {
+                        Console.WriteLine("Incorrect input");
+                    }
+                    else
+                    {
+                        break;
+                    }                   
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input");
+                }
+            }
+            while (true) // Sums 2 random numbers till the desirable sum is reached
+            {
+                int roll1 = rand.Next(1, 7);
+                int roll2 = rand.Next(1, 7);
+                int rollSum = roll1 + roll2;
+                Console.WriteLine($"{roll1} and {roll2} = {rollSum}");
+                if (rollSum == desirableSum)
+                {
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
