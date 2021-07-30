c2b9be1c6fbb837b8d02b4726c88aaba5f4b26a6
KrEdu <kristians12x@gmail.com>
2021-07-30 12:51:27 +0300

    Exercise7 done

---
+++ csharp-basics/exercises/Loops/Loops/Exercise7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise7
+{
+    class Piglet
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Piglet!");
+            var rand = new Random();
+            int sum = 0;
+            while (true)
+            { 
+                int diceResult = rand.Next(1, 7);
+                Console.WriteLine($"You rolled a {diceResult}");
+                if (diceResult != 1)
+                {                    
+                    sum += diceResult;
+                    Console.WriteLine("Do you want to roll again? (y/n)");
+                    string userInput = Console.ReadLine();                    
+                    if (userInput == "n" || userInput == "N")
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    sum = 0;
+                    break;
+                }                
+            }
+            Console.WriteLine($"You got {sum} points");
+            Console.ReadKey();
+        }
+    }
+}
