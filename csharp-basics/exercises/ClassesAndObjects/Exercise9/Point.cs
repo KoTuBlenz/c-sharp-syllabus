e5768f422cd0b178c6a3d82e4a8cd84cb456a6eb
KrEdu <kristians12x@gmail.com>
2021-08-12 23:30:46 +0300

    Exercise9 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise9/Point.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Exercise9
+{
+    class Point
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public Point(int X, int Y)
+        {
+            x = X;
+            y = Y;
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(5, 2);
+            Point p2 = new Point(-3, 6);
+            swapPoints(p1, p2);
+            Console.WriteLine("(" + p1.x + ", " + p1.y + ")");
+            Console.WriteLine("(" + p2.x + ", " + p2.y + ")");
+            Console.ReadKey();
+        }
+
+        static void swapPoints(Point point1, Point point2)
+        {
+            int point1X = point1.x;
+            int point1Y = point1.y;
+            point1.x = point2.x;
+            point1.y = point2.y;
+            point2.x = point1X;
+            point2.y = point1Y;
+        }
+    }
+}
