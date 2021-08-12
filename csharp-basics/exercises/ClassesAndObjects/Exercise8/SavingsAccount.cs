c49ebca57fcc95433246020e32dbf38354396aef
KrEdu <kristians12x@gmail.com>
2021-08-12 23:30:30 +0300

    Exercise8 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise8/SavingsAccount.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Exercise8
+{
+    class SavingsAccount
+    {
+        private double _balance;
+        private double _monthlyInterestRate;
+        private double _interestEarned;
+        private double _totalAmountDeposited;
+        private double _totalAmountWithdrawn;
+        public SavingsAccount(double startingBalance, double annualInterestRate)
+        {
+            _balance = startingBalance;
+            _monthlyInterestRate = annualInterestRate/12;
+        }
+        public void Withdrawn(double amountWithdrawn)
+        {
+            _totalAmountWithdrawn += amountWithdrawn;  
+            _balance -= amountWithdrawn;
+        }
+        public void Deposit(double amountOfDeposit)
+        {
+            _totalAmountDeposited += amountOfDeposit;
+            _balance += amountOfDeposit;
+        }
+        public void MonthlyInterest()
+        {
+        _interestEarned += _balance * _monthlyInterestRate;
+            _balance += _balance * _monthlyInterestRate;
+        }
+        public void PrintBalance()
+        {
+            Console.WriteLine($"Ending balance is: {String.Format("{0:n}", _balance)}");
+        }
+        public void PrintInterestEarned()
+        {
+            Console.WriteLine($"Interest earned is: {String.Format("{0:n}", _interestEarned)}");
+        }
+        public void PrintTotalAmountDeposited()
+        {
+            Console.WriteLine($"Total amount deposited is: {String.Format("{0:n}", _totalAmountDeposited)}");
+        }
+        public void PrintTotalAmountWithdrawn()
+        {
+            Console.WriteLine($"Total amount withdrawn is: {String.Format("{0:n}", _totalAmountWithdrawn)}");
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise8/SavingsAccountTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How much money is in the account?: ");
+            double startingBalance = double.Parse(Console.ReadLine());
+            Console.Write("Enter the annual interest rate: ");
+            double annualInterestsRate = double.Parse(Console.ReadLine());
+            SavingsAccount savingsAccount1 = new SavingsAccount(startingBalance, annualInterestsRate);
+            Console.Write("How long has the account been opened?: ");
+            int months = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= months; i++)
+            {
+                Console.Write($"Enter amount deposited for month {i}:");
+                savingsAccount1.Deposit(double.Parse(Console.ReadLine()));
+                Console.Write($"Enter amount withdrawn for month {i}:");
+                savingsAccount1.Withdrawn(double.Parse(Console.ReadLine()));
+                savingsAccount1.MonthlyInterest();
+            }
+            Console.WriteLine();
+            savingsAccount1.PrintTotalAmountDeposited();
+            savingsAccount1.PrintTotalAmountWithdrawn();
+            savingsAccount1.PrintInterestEarned();
+            savingsAccount1.PrintBalance();
+            Console.ReadKey();
+        }
+    }
+
+}
