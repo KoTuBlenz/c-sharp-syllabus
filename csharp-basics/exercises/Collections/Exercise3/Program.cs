022561b78978786f41d7c995b435599c0b9b0e09
KrEdu <kristians12x@gmail.com>
2021-08-15 15:51:42 +0300

    Exercise3 done

---
+++ csharp-basics/exercises/Collections/Exercise3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new HashSet<string>();
+            while (true)
+            {
+                Console.Write("Enter name:");
+                string name = Console.ReadLine();
+                if (name == "") break;
+                names.Add(name);
+            }
+            Console.WriteLine($"Unique name list contains: {String.Join(" ",names)}");
+            Console.ReadKey();
+        }
+    }
+}
