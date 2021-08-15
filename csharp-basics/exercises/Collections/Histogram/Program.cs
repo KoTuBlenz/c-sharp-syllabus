e1f2ed798064936efe4896e1a2793d8ad8323485
KrEdu <kristians12x@gmail.com>
2021-08-15 15:53:46 +0300

    Exercise7 done

--- csharp-basics/exercises/Collections/Histogram/Program.cs
+++ csharp-basics/exercises/Collections/Histogram/Program.cs
@@ -1,23 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Histogram
 {
     class Program
     {
-        private const string Path = "../Histogram/midtermscores.txt";
-
+        private const string Path = "../../midtermscores.txt";
+        private static SortedDictionary<int,int>ScoreSortedList = new SortedDictionary<int, int>();
         private static void Main(string[] args)
         {
-            var readText = File.ReadAllLines(Path);
+            var readText = File.ReadAllText(Path).Split(' ');
+            //Fills sorted dictionary with keys from 0 to 10 with 0 value
+            for (int i = 0; i<11; i++)
+            {
+                ScoreSortedList.Add(i,0);
+            }
+            //Divides every number by 10, rounds it down and then increases the keys value by 1
             foreach (var s in readText)
             {
-                Console.WriteLine(s);
+                int num = int.Parse(s);
+                int key = num / 10;
+                ScoreSortedList[key]++;
             }
+            foreach(KeyValuePair<int,int> kvp in ScoreSortedList)
+            {
+                if (kvp.Key == 10)
+                {
+                    Console.WriteLine($"  {string.Format("{0:00}", kvp.Key * 10)}: {new string('*', kvp.Value)}");
+                }
+                else
+                {
+                    Console.WriteLine($"{string.Format("{0:00}", kvp.Key * 10)}-{string.Format("{0:00}", (kvp.Key * 10) + 9)}: {new string('*', kvp.Value)}");
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
