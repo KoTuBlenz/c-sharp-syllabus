0827256cc51ba59bc041796826effcfc76a5857b
KrEdu <kristians12x@gmail.com>
2021-08-15 15:54:29 +0300

    Exercise9 done

---
+++ csharp-basics/exercises/Collections/Exercise9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomList = new HashSet<string>() { "Apple", "Pineapple", "Cybertruck", "MacBook", "Android" };
+            Console.WriteLine("List with 5 items");
+            foreach(string thing in randomList)
+            {
+                Console.WriteLine(thing);
+            }
+            randomList.Clear();
+            randomList.Add("Mac and cheese");
+            randomList.Add("Mac and cheese");
+            Console.WriteLine("\nList with duplicate items");
+            foreach (string thing in randomList)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.ReadKey();
+        }
+    }
+}
