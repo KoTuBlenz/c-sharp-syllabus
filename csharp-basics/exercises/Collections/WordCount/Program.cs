b210c6024889b3564270571b36912fe56e013265
KrEdu <kristians12x@gmail.com>
2021-08-15 15:52:38 +0300

    Exercise5 done

--- csharp-basics/exercises/Collections/WordCount/Program.cs
+++ csharp-basics/exercises/Collections/WordCount/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace WordCount
 {
@@ -10,7 +7,26 @@ namespace WordCount
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = "../../lear.txt";
+            Console.WriteLine("Lines in text: "+LinesInText(path));
+            Console.WriteLine("Words in text: " + WordsInText(path));
+            Console.WriteLine("Chars in text: " + CharsInText(path));
+            Console.ReadKey();
+        }
+
+        public static int LinesInText(string path)
+        {
+            return File.ReadAllLines(path).Length;
+        }
+
+        public static int WordsInText(string path)
+        {
+            return File.ReadAllText(path).Replace("'", " ").Replace("  ", " ").Split(' ', '\n').Length;
+        }
+
+        public static int CharsInText(string path)
+        {
+            return File.ReadAllText(path).Replace("\n","").ToCharArray().Length;
         }
     }
 }
