c2c096e2a6edaeaa18b8e53bbc535e7fd7576f4f
KrEdu <kristians12x@gmail.com>
2021-08-21 14:42:55 +0300

    Exercise 2 done

--- csharp-basics/exercises/Polymorphism/MakeSounds/Program.cs
+++ csharp-basics/exercises/Polymorphism/MakeSounds/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MakeSounds
 {
@@ -6,7 +7,9 @@ namespace MakeSounds
     {
         private static void Main(string[] args)
         {
-            
+            var soundList = new List<ISound>() { new Firework(), new Parrot(), new Radio(), new Firework(), new Radio() };
+            soundList.ForEach(sound => sound.PlaySound());
+            Console.ReadKey();
         }
     }
 }
