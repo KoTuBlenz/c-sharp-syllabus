5a850455e07606cc56f792bd659f0869af1e71a3
KrEdu <kristians12x@gmail.com>
2021-08-21 14:44:18 +0300

    Exercise 5 done

--- csharp-basics/exercises/Polymorphism/AdApp/Hoarding.cs
+++ csharp-basics/exercises/Polymorphism/AdApp/Hoarding.cs
@@ -2,17 +2,21 @@ namespace AdApp
 {
     public class Hoarding: Advert
     {
-        private int rate;
+        private int _rate;
         //per day
-        private int numDays;
-
-        public Hoarding(int fee) : base(fee)
+        private int _numDays;
+        private bool _primeLocation;
+        public Hoarding(int fee, int numDays, int rate, bool primeLocation) : base(fee)
         {
+            _rate = rate;
+            _numDays = numDays;
+            _primeLocation = primeLocation;
         }
 
-        public new int Cost() 
+        public override int Cost() 
         {
-            return base.Cost();
+            int addPrice = _primeLocation ? (int)(_rate * _numDays * 1.5) : _rate * _numDays;
+            return base.Cost() + addPrice;
         }
 
         public override string ToString() 
---
+++ csharp-basics/exercises/Polymorphism/AdApp/Poster.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdApp
+{
+    class Poster: Advert
+    {
+        private int _height;
+        private int _width;
+        private int _amount;
+        private int _costPerSize;
+
+        public Poster(int fee, int height, int width, int amount, int costPerSize) : base(fee)
+        {
+            _height = height;
+            _width = width;
+            _amount = amount;
+            _costPerSize = costPerSize;
+        }
+
+        public override int Cost()
+        {
+            int addPrice = _height*_width* _costPerSize * _amount;
+            return base.Cost() + addPrice;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
