f403fa18969c07f085918b42543ce7b0bb631d33
KrEdu <kristians12x@gmail.com>
2021-08-21 14:43:48 +0300

    Exercise 4 done

---
+++ csharp-basics/exercises/Polymorphism/Firm/Commission .cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Firm
+{
+    class Commission :Hourly
+    {
+        private double _commissionRate;
+        private double _totalSales;
+        public Commission(string eName, string eAddress, string ePhone, string socSecNumber, double rate, double commissRate) :base(eName,eAddress,ePhone,socSecNumber,rate)
+        {
+            _commissionRate = commissRate;
+        }
+        public void AddSales(double totalSales)
+        {
+            _totalSales += totalSales;
+        }
+        public override double Pay()
+        {
+            var payment = base.Pay() + (_totalSales * _commissionRate);
+            _totalSales = 0;
+            return payment;
+        }
+        public override string ToString()
+        {
+            var result = base.ToString();
+            result += "\nTotal Sales: " + _totalSales;
+            return result;
+        }
+    }
+}
