89fa669212a98b6f4659f70c11a6cdf22e3de1b7
KrEdu <kristians12x@gmail.com>
2021-08-21 14:41:17 +0300

    Exercise 1 done

---
+++ csharp-basics/exercises/Polymorphism/DragRace/Opel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DragRace
+{
+    public class Opel : ICar
+    {
+        private int currentSpeed = 0;
+        public string name { get; set; }
+
+        public void SpeedUp()
+        {
+            currentSpeed += 13;
+        }
+
+        public void SlowDown()
+        {
+            currentSpeed -= 20;
+        }
+
+        public string ShowCurrentSpeed()
+        {
+            return currentSpeed.ToString();
+        }
+
+        public void StartEngine()
+        {
+            Console.WriteLine("Brum.........");
+        }
+    }
+}
---
+++ csharp-basics/exercises/Polymorphism/DragRace/Volvo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DragRace
+{
+    class Volvo : ICar, ICarBoost
+    {
+        private int currentSpeed = 0;
+        public string name { get; set; }
+
+        public void SpeedUp()
+        {
+            currentSpeed += 12;
+        }
+
+        public void SlowDown()
+        {
+            currentSpeed -= 22;
+        }
+
+        public string ShowCurrentSpeed()
+        {
+            return currentSpeed.ToString();
+        }
+
+        public void UseNitrousOxideEngine()
+        {
+            currentSpeed += 10;
+        }
+        public void StartEngine()
+        {
+            Console.WriteLine("Rrrrrrr.....");
+        }
+    }
+}
