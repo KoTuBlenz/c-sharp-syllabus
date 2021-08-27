2c6e2bbfeb357a5839ba18ed8e97fb0df308f36e
KrEdu <kristians12x@gmail.com>
2021-08-27 23:10:40 +0300

    Arithmetic Exercise 6 test done

---
+++ csharp-basics/exercises/Arithmetic/CozaLozaWoza/CozaLozaWozaTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CozaLozaWozaTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CozaLozaWozaTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a96c690f-42c0-4ee4-b78f-837eccf64d11")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- csharp-basics/exercises/Arithmetic/CozaLozaWoza/Program.cs
+++ csharp-basics/exercises/Arithmetic/CozaLozaWoza/Program.cs
@@ -10,43 +10,75 @@ namespace CozaLozaWoza
     {
         static void Main(string[] args)
         {
+           // var target = new CozaLozaWoza();
+            var result = "";
             for (int i = 1; i <= 110; i++)
-            {              
-                string answer = "";
-                if (i % 3 == 0)
-                {
-                    answer += "Coza";
-                }
-
-                if (i % 5 == 0)
-                {
-                    answer += "Loza";
-                }
-
-                if (i % 7 == 0)
-                {
-                    answer += "Woza";
-                }
-
-                if (answer != "")
-                {
-                    Console.Write(answer);
-                }
-                else
-                {
-                    Console.Write(i);
-                }
-
+            {
+                result += CozaLozaWoza.CheckNumber(i);
                 if (i % 11 == 0)
                 {
-                    Console.WriteLine();
+                    result += "\n";
                 }
                 else
                 {
-                    Console.Write(" ");
+                    result += " ";
                 }
             }
+            //var cozaLozaWoza = new CozaLozaWoza(110);
+            //cozaLozaWoza.DisplayCozaLozaWoza();
+            Console.WriteLine(result);
             Console.ReadKey();
         }
+    }
+    public class CozaLozaWoza
+    {
+        //private string _cozaLozaWozaString;
+
+        //public CozaLozaWoza(int number)
+        //{
+        //    for (int i = 1; i<=number; i++)
+        //    {
+        //        _cozaLozaWozaString += CheckNumberForCozaLozaWoza(i);
+        //        if (i % 11 == 0)
+        //        {
+        //            _cozaLozaWozaString += "\n";
+        //        }
+        //        else
+        //        {
+        //            _cozaLozaWozaString += " ";
+        //        }
+        //    }
+        //}
+        //public CozaLozaWoza() { }
+
+        public static string CheckNumber(int number)
+        {
+            string answer = "";
+            if (number % 3 == 0)
+            {
+                answer += "Coza";
+            }
+
+            if (number % 5 == 0)
+            {
+                answer += "Loza";
+            }
+
+            if (number % 7 == 0)
+            {
+                answer += "Woza";
+            }
+
+            if (answer.Length == 0)
+            {
+                answer = number.ToString();
+            }
+            return answer;
+        }
+
+        //public void DisplayCozaLozaWoza()
+        //{
+        //    Console.WriteLine(_cozaLozaWozaString);
+        //}
     }
 }
