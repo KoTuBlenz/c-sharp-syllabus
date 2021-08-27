9ee826796d4a036411ab02a55793cbbcb16058f4
KrEdu <kristians12x@gmail.com>
2021-08-27 23:08:50 +0300

    Arithmetics Exercise2 test done

---
+++ csharp-basics/exercises/Arithmetic/Exercise2/Exercise2Test/Exercise2Test.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Exercise2.Test
+{
+    [TestClass]
+    public class Exercise2Test
+    {
+        [TestMethod]
+        public void ReturnOddEven_OddNumber_ExpectedStringOddNumber()
+        {
+            //Arange
+            var number = 1;
+            //Act
+            var result = CheckOddEven.ReturnOddEven(number);
+            //Assert
+            Assert.AreEqual("Odd number", result);
+        }
+
+        [TestMethod]
+        public void ReturnOddEven_EvenNumber_ExpectedStringEvenNumber()
+        {
+            //Arange
+            var number = 2;
+            //Act
+            var result = CheckOddEven.ReturnOddEven(number);
+            //Assert
+            Assert.AreEqual("Even number", result);
+        }
+    }
+}
---
+++ csharp-basics/exercises/Arithmetic/Exercise2/Exercise2Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Exercise2Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exercise2Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0efdd9ad-5d2d-47dc-b559-cfb14e584949")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
