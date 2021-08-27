b304ecb8d37bcd022eac06f4c7413545007c802b
KrEdu <kristians12x@gmail.com>
2021-08-27 23:11:22 +0300

    Arithmetic exercise 7 test done

---
+++ csharp-basics/exercises/Arithmetic/GravityCalculator/GravityCalculatorTest/GravityCalculatorTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GravityCalculator.Test
+{
+    [TestClass]
+    public class GravityCalculatorTest
+    {
+        [DataTestMethod]
+        [DataRow(-9.81, 0, 10.0 ,0, -490.5)]
+        [DataRow(-9.81, 0, 10.0, 10, -480.5)]
+        [DataRow(-9.81, 20.0, 10.0, 0, -290.5)]
+        [DataRow(-5.0, 0.0, 10.0, 0, -250)]
+
+        public void CalculateFinalPosition_DifferentVarialbes_ExpetedOutputIsReturned
+            (double gravity, double initialVelocity, double fallingTime, double initialPosition, double expected)
+        {
+            //Act
+            var result = GravityCalculator.CalculateFinalPosition(gravity, initialVelocity, fallingTime, initialPosition);
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
