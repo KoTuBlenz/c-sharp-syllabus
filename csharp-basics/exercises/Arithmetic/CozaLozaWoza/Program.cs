33846c2bf9c5bdaa28ee11013e205b7a85fc1c25
KrEdu <kristians12x@gmail.com>
2021-08-30 12:54:32 +0300

    Removed unnecessary comments

--- csharp-basics/exercises/Arithmetic/CozaLozaWoza/Program.cs
+++ csharp-basics/exercises/Arithmetic/CozaLozaWoza/Program.cs
@@ -10,7 +10,6 @@ namespace CozaLozaWoza
     {
         static void Main(string[] args)
         {
-           // var target = new CozaLozaWoza();
             var result = "";
             for (int i = 1; i <= 110; i++)
             {
@@ -24,33 +23,12 @@ namespace CozaLozaWoza
                     result += " ";
                 }
             }
-            //var cozaLozaWoza = new CozaLozaWoza(110);
-            //cozaLozaWoza.DisplayCozaLozaWoza();
             Console.WriteLine(result);
             Console.ReadKey();
         }
     }
     public class CozaLozaWoza
     {
-        //private string _cozaLozaWozaString;
-
-        //public CozaLozaWoza(int number)
-        //{
-        //    for (int i = 1; i<=number; i++)
-        //    {
-        //        _cozaLozaWozaString += CheckNumberForCozaLozaWoza(i);
-        //        if (i % 11 == 0)
-        //        {
-        //            _cozaLozaWozaString += "\n";
-        //        }
-        //        else
-        //        {
-        //            _cozaLozaWozaString += " ";
-        //        }
-        //    }
-        //}
-        //public CozaLozaWoza() { }
-
         public static string CheckNumber(int number)
         {
             string answer = "";
@@ -75,10 +53,5 @@ namespace CozaLozaWoza
             }
             return answer;
         }
-
-        //public void DisplayCozaLozaWoza()
-        //{
-        //    Console.WriteLine(_cozaLozaWozaString);
-        //}
     }
 }
